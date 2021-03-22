c29184d55229fb9e6fa86aad908c4e1a5b2b7fb0
kdy <1402039615@qq.com>
2021-03-22 19:09:41 +0800

    DEA 数据增强

---
+++ EDA.py
@@ -0,0 +1,7 @@
+from textda.data_expansion import *
+
+print(data_expansion('弄是来工作额伐？'))
+print(data_expansion('吾是伊额同学刘刚。'))
+print(data_expansion('祝侬学习取得更好额成绩。'))
+print(data_expansion('伊拉蛮好，老好额！'))
+print(data_expansion('拿爸爸妈妈呢， 伊拉好伐？'))
